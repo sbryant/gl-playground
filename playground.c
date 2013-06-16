f66acad3ae409887ad709269e3169396d968771a
Sean Bryant <sbryant@github.com>
2013-06-15 20:03:40 -0700

    Go go gadget window.

--- playground.c
+++ playground.c
@@ -47,7 +47,7 @@ int main(int argc, char** argv) {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
 	if (!window) {
 		goto error;
 	}
