365f5022f1dec33c804bdd9879511eecadbabc65
Sean Bryant <sbryant@github.com>
2013-06-22 20:18:59 -0700

    some matrix functions and helpers.

--- src/utils.c
+++ src/utils.c
@@ -23,3 +23,38 @@ char *read_shader(const char* path) {
 
 	return result;
 };
+
+void mat4_rotate(mat4 model, float angle, vec3 axis, mat4 result) {
+
+    float rads = angle / 180.0 * M_PI;
+    mat4 rotate_mat;
+
+    if(axis[0] == 1.0) {
+        mat4 temp = MAT4_ROTATE_X(rads);
+        memcpy(rotate_mat, temp, sizeof(mat4));
+    }
+
+    else if(axis[1] == 1.0) {
+        mat4 temp = MAT4_ROTATE_Y(rads);
+        memcpy(rotate_mat, temp, sizeof(mat4));
+    }
+    else if(axis[2] == 1.0) {
+        mat4 temp = MAT4_ROTATE_Z(rads);
+        memcpy(rotate_mat, temp, sizeof(mat4));
+    }
+
+    mat4_mul(model, rotate_mat, result);
+}
+
+void mat4_mul(mat4 a, mat4 b, mat4 result) {
+    /* go through the rows multiplying the rows of by the columns of be and adding the sums */
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            float temp = 0.0;
+            for(int k = 0; k < 4; k++) {
+                temp += a[i][k] * b[k][j];
+            }
+            result[i][j] = temp;
+        }
+    }
+}
--- src/utils.h
+++ src/utils.h
@@ -3,11 +3,52 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
 #ifndef __GLPG_UTILS_H__
 #define __GLPG_UTILS_H__
 
+typedef float vec4[4];
+typedef float vec3[3];
+typedef vec4 mat4[4];
+
+#define MAT4_IDENTITY           \
+    {                           \
+        { 1.0, 0.0, 0.0, 0.0 }, \
+        { 0.0, 1.0, 0.0, 0.0 }, \
+        { 0.0, 0.0, 1.0, 0.0 }, \
+        { 0.0, 0.0, 0.0, 1.0 }  \
+    }
+
+#define VEC3_ROTATE_X { 1.0, 0.0, 0.0 }
+#define VEC3_ROTATE_Y { 0.0, 1.0, 0.0 }
+#define VEC3_ROTATE_Z { 0.0, 0.0, 1.0 }
+
+#define MAT4_ROTATE_X(rads) { \
+    { 1.0, 0.0, 0.0, 0.0 },              \
+    { 0.0, cos(rads), sin(rads), 0.0 },  \
+    { 0.0, -sin(rads), cos(rads), 0.0 }, \
+    { 0.0, 0.0, 0.0, 1.0 }               \
+}
+
+#define MAT4_ROTATE_Y(rads) { \
+    { cos(rads), 0.0, -sin(rads), 0.0 }, \
+    { 0.0, 1.0, 0.0, 0.0 },              \
+    { sin(rads), 0, cos(rads), 0.0 },    \
+    { 0.0, 0.0, 0.0, 1.0 }               \
+}
+
+#define MAT4_ROTATE_Z(rads) { \
+    { cos(rads), sin(rads), 0.0, 0.0 },  \
+    { -sin(rads), cos(rads), 0.0, 0.0 }, \
+    { 0.0, 0.0, 1.0, 0.0 },              \
+    { 0.0, 0.0, 0.0, 1.0 }               \
+}
+
 #define GLPG_SHADER_READ_SIZE 1024
 
 char *read_shader(const char* path);
+
+void mat4_rotate(mat4 model, float angle, vec3 rotation_axis, mat4 result);
+void mat4_mul(mat4 a, mat4 b, mat4 result);
 #endif
