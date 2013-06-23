34963356a865af62ae71fed427227cd7d3e03bf2
Sean Bryant <sbryant@github.com>
2013-06-23 10:40:07 -0700

    Be more explicit about view matrix values.

--- src/utils.c
+++ src/utils.c
@@ -50,10 +50,13 @@ void mat4_lookat(vec3 eye, vec3 center, vec3 up, mat4 result) {
 
     vec3 z_axis; vec3 x_axis; vec3 y_axis;
     vec3 eye_sub_center; vec3 up_cross_z;
+
     vec3_sub(eye, center, eye_sub_center);
     vec3_normalize(eye_sub_center, z_axis);
+
     vec3_cross(up, z_axis, up_cross_z);
     vec3_normalize(up_cross_z, x_axis);
+
     vec3_cross(z_axis, x_axis, y_axis);
 
     result[0][0] = x_axis[0];
@@ -68,10 +71,14 @@ void mat4_lookat(vec3 eye, vec3 center, vec3 up, mat4 result) {
     result[1][2] = z_axis[1];
     result[2][2] = z_axis[2];
 
+    result[0][3] = 0.0;
+    result[1][3] = 0.0;
+    result[2][3] = 0.0;
+    result[3][3] = 1.0;
+
     result[3][0] = vec3_dot(x_axis, eye);
     result[3][1] = vec3_dot(y_axis, eye);
     result[3][2] = vec3_dot(z_axis, eye);
-    result[3][3] = 1.0;
 }
 
 void vec3_sub(vec3 a, vec3 b, vec3 res) {
