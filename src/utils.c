e04988788d67e1deeebead2354c871414c109bd1
Sean Bryant <sbryant@github.com>
2013-06-30 09:10:18 -0700

    Explicit pointer usage for matrix functions.

--- src/utils.c
+++ src/utils.c
@@ -24,29 +24,29 @@ char *read_shader(const char* path) {
 	return result;
 };
 
-void mat4_rotate(mat4 model, float angle, vec3 axis, mat4 result) {
+void mat4_rotate(mat4 *model, float angle, vec3 *axis, mat4 *result) {
 
     float rads = (angle * M_PI) / 180.0;
     mat4 rotate_mat;
 
-    if(axis[0] == 1.0) {
+    if((*axis)[0] == 1.0) {
         mat4 temp = MAT4_ROTATE_X(rads);
         memcpy(rotate_mat, temp, sizeof(mat4));
     }
 
-    else if(axis[1] == 1.0) {
+    else if((*axis)[1] == 1.0) {
         mat4 temp = MAT4_ROTATE_Y(rads);
         memcpy(rotate_mat, temp, sizeof(mat4));
     }
-    else if(axis[2] == 1.0) {
+    else if((*axis)[2] == 1.0) {
         mat4 temp = MAT4_ROTATE_Z(rads);
         memcpy(rotate_mat, temp, sizeof(mat4));
     }
 
-    mat4_mul(model, rotate_mat, result);
+    mat4_mul(model, &rotate_mat, result);
 }
 
-void mat4_lookat(vec3 *eye, vec3 *center, vec3 *up, mat4 result) {
+void mat4_lookat(vec3 *eye, vec3 *center, vec3 *up, mat4 *result) {
 
     vec3 z_axis; vec3 x_axis; vec3 y_axis;
     vec3 eye_sub_center; vec3 up_cross_z;
@@ -70,27 +70,22 @@ void mat4_lookat(vec3 *eye, vec3 *center, vec3 *up, mat4 result) {
 
     vec3_cross(&z_axis, &x_axis, &y_axis);
 
-    result[0][0] = x_axis[0];
-    result[1][0] = y_axis[0];
-    result[2][0] = z_axis[0];
+    (*result)[0][0] = x_axis[0];
+    (*result)[1][0] = y_axis[0];
+    (*result)[2][0] = z_axis[0];
 
-    result[0][1] = x_axis[1];
-    result[1][1] = y_axis[1];
-    result[2][1] = z_axis[1];
+    (*result)[0][1] = x_axis[1];
+    (*result)[1][1] = y_axis[1];
+    (*result)[2][1] = z_axis[1];
 
-    result[0][2] = x_axis[2];
-    result[1][2] = y_axis[2];
-    result[2][2] = z_axis[2];
+    (*result)[0][2] = x_axis[2];
+    (*result)[1][2] = y_axis[2];
+    (*result)[2][2] = z_axis[2];
 
-    result[0][3] = 0.0;
-    result[1][3] = 0.0;
-    result[2][3] = 0.0;
-    result[3][3] = 1.0;
-
-    /*
-    result[3][0] = vec3_dot(x_axis, eye);
-    result[3][1] = vec3_dot(y_axis, eye);
-    result[3][2] = vec3_dot(z_axis, eye); */
+    (*result)[0][3] = 0.0;
+    (*result)[1][3] = 0.0;
+    (*result)[2][3] = 0.0;
+    (*result)[3][3] = 1.0;
 }
 
 float vec3_length(vec3 *a) {
@@ -122,7 +117,7 @@ float vec3_dot(vec3 *a, vec3 *b) {
     return ((*a)[0]*(*b)[0]) + ((*a)[1]*(*b)[1]) + ((*a)[2]*(*b)[2]);
 }
 
-void mat4_frustum(float left, float right, float bottom, float top, float near, float far, mat4 result) {
+void mat4_frustum(float left, float right, float bottom, float top, float near, float far, mat4 *result) {
     float x = 2 * near / (right - left);
     float y = 2 * near / (top - bottom);
 
@@ -131,14 +126,14 @@ void mat4_frustum(float left, float right, float bottom, float top, float near, 
     float c = - (far + near) / (far - near);
     float d = -2 * far * near / (far - near);
 
-    result[0][0] = x; result[0][2] = a;
-    result[1][1] = y; result[1][2] = b;
-    result[2][2] = c; result[2][3] = d;
-    result[3][2] = -1; result[3][3] = 0;
+    (*result)[0][0] = x; (*result)[0][2] = a;
+    (*result)[1][1] = y; (*result)[1][2] = b;
+    (*result)[2][2] = c; (*result)[2][3] = d;
+    (*result)[3][2] = -1; (*result)[3][3] = 0;
 
 }
 
-void mat4_perspective(float fovy, float w, float h, float z_near, float z_far, mat4 result) {
+void mat4_perspective(float fovy, float w, float h, float z_near, float z_far, mat4 *result) {
     float ymax = z_near * tan(fovy * 0.5);
     float ymin = -ymax;
     float aspect = w/h;
@@ -159,13 +154,13 @@ void mat4_perspective(float fovy, float w, float h, float z_near, float z_far, m
     result[3][2] = -1.0; */
 }
 
-void mat4_mul(mat4 a, mat4 b, mat4 result) {
+void mat4_mul(mat4 *a, mat4 *b, mat4 *result) {
     /* go through the rows multiplying the rows of by the columns of be and adding the sums */
     for(int i = 0; i < 4; i++) {
         for(int j = 0; j < 4; j++) {
-            result[i][j] =  0.0;
+            (*result)[i][j] =  0.0;
             for(int k = 0; k < 4; k++) {
-                result[i][j] += a[k][j] * b[i][k];
+                (*result)[i][j] += (*a)[k][j] * (*b)[i][k];
             }
         }
     }
